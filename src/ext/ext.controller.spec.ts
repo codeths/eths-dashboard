72adc9195b42ee4cea8a63b75a686df9c636fc69
mswider <37093293+mswider@users.noreply.github.com>
2023-10-27 15:29:52 +0000

    fix: make tests load

--- src/ext/ext.controller.spec.ts
+++ src/ext/ext.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExtController } from './ext.controller';
+import { ConfigService } from '@nestjs/config';
+import { ExtService } from './ext.service';
 
 describe('ExtController', () => {
   let controller: ExtController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [ConfigService, ExtService],
       controllers: [ExtController],
     }).compile();
 
--- src/ext/ext.service.spec.ts
+++ src/ext/ext.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExtService } from './ext.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('ExtService', () => {
   let service: ExtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExtService],
+      providers: [ConfigService, ExtService],
     }).compile();
 
     service = module.get<ExtService>(ExtService);
