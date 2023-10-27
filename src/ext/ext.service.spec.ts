0951198e625ecf2076c886c019ef7b99d8d3bf79
mswider <37093293+mswider@users.noreply.github.com>
2023-10-27 20:15:55 +0000

    test(ext): create tests for service

--- src/ext/ext.service.spec.ts
+++ src/ext/ext.service.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExtService } from './ext.service';
 import { ConfigService } from '@nestjs/config';
+import axios, { AxiosError } from 'axios';
+import { GatewayTimeoutException } from '@nestjs/common';
+
+jest.mock('axios');
 
 describe('ExtService', () => {
   let service: ExtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ConfigService, ExtService],
+      providers: [
+        ExtService,
+        {
+          provide: ConfigService,
+          useValue: {
+            getOrThrow: jest.fn(() => ''),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ExtService>(ExtService);
@@ -15,5 +27,33 @@ describe('ExtService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('should return a response', () => {
+    const data = {
+      object: {
+        serial: 'NXHQEAA0010241CC467600',
+        deviceStatus: 'Given to Assignee',
+        loanerStatus: 'Not A Loaner',
+        startDate: '2020-07-14T09:50:05.487',
+      },
+      message: null,
+      success: true,
+    };
+    const response = { data };
+
+    const mockedAxios = jest.mocked(axios);
+    mockedAxios.get.mockResolvedValue(response);
+
+    expect(service.getResponseFromOneToOne('')).resolves.toStrictEqual(
+      response,
+    );
+  });
+  it('should throw on timeouts', () => {
+    const mockedAxios = jest.mocked(axios);
+    mockedAxios.get.mockRejectedValue(new AxiosError());
+
+    expect(async () => {
+      await service.getResponseFromOneToOne('');
+    }).rejects.toThrow(GatewayTimeoutException);
   });
 });
